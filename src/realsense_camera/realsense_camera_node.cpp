3d05e0bd0220629eaefca5dcd92d1eaf9f1685dd
abhishek_padalkar <abhishek.padalkar@smail.inf.h-brs.de>
2018-12-17 00:29:48 +0100

    example node code cleanup

--- src/realsense_camera/realsense_camera_node.cpp
+++ src/realsense_camera/realsense_camera_node.cpp
@@ -8,19 +8,14 @@ int main(int argc, char *argv[])
     std::shared_ptr<RealsenseCamera> device = std::make_shared<RealsenseCamera>();
     ros::init(argc, argv, "example_node_1");
     ros::NodeHandle nh_;
-    device->initialize();
     pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
     ptr_cloud cloud;
     ptr_cloud cloud_new;
     rs2::frame color;
-    rs2::frame depth;
-    // zmq::context_t context (1);
-    // zmq::socket_t publisher (context, ZMQ_PUB);
-    // publisher.bind("ipc:///tmp/cloud.ipc");  
+    rs2::frame depth; 
     double t_start;
     double t_end;
-    // boost::interprocess::managed_shared_memory shared_point_cloud_memory(boost::interprocess::open_or_create, "shared_point_cloud_memory", 100240);
-    // point_cloud *shared_point_cloud = shared_point_cloud_memory.find_or_construct<point_cloud>("point_cloud")();
+
     while (nh_.ok())
     {
         ptr_cloud cloud_new(new point_cloud);
@@ -32,29 +27,5 @@ int main(int argc, char *argv[])
         ros::Rate r(30);
         r.sleep();
         viewer.showCloud(cloud_new);
-        // shared_point_cloud_memory.find_or_construct<point_cloud>("point_cloud")(*cloud);
-        // point_cloud *pt_cloud;
-        // pt_cloud = cloud.get();
-        // try
-        // {
-        //     zmq::message_t point_cloud_msg(5 * sizeof(*pt_cloud));
-        //     memcpy(point_cloud_msg.data(), pt_cloud, 5 * sizeof (*pt_cloud));
-        //     publisher.send(point_cloud_msg);
-        // }
-        // catch (std::exception e1)
-        // {
-        // try 
-        // {
-        //     zmq::message_t point_cloud_msg(sizeof("hi"));
-        //     memcpy(point_cloud_msg.data(), "hi", sizeof ("hi"));
-        //     publisher.send(point_cloud_msg);
-        //     // std::cout << "msg sent" << std::endl;
-
-        // }
-        // catch (std::exception e1)
-        // {
-        //     // publisher.bind("ipc://point_cloud.ipc");      
-        //     std::cout << "failed" << std::endl;
-        // }
     }
 }
--- src/realsense_camera/realsense_camera_node_2.cpp
+++ src/realsense_camera/realsense_camera_node_2.cpp
@@ -1,43 +1,9 @@
 #include "realsense_camera_device_wrapper.h"
 #include <librealsense2/rs.hpp>
 #include <ros/ros.h>
-#include <boost/interprocess/managed_shared_memory.hpp>
-#include <zmq.hpp>
 
-// #include <boost/shared_pointer.hpp>
 #include <iostream>
 int main(int argc, char *argv[]) 
 {
-    ros::init(argc, argv, "example_node_2");
-    std::cout << "connected";
-    ros::NodeHandle nh_;
-    // pcl::visualization::CloudViewer viewer("Simple Cloud Viewer 2");
-    ptr_cloud cloud;
-    rs2::frame color;
-    rs2::frame depth;
-    zmq::context_t context (1);
-    zmq::socket_t subscriber (context, ZMQ_SUB);
-    std::cout << "connected";
-    subscriber.connect("ipc:///tmp/cloud.ipc");
-    zmq_connect(subscriber, "ipc:///tmp/cloud.ipc");
-    std::cout << "connected";
-    ROS_WARN ("connected");
-
-    // boost::interprocess::managed_shared_memory shared_point_cloud_memory(boost::interprocess::open_or_create, "shared_point_cloud_memory", 10240);
-    // point_cloud *shared_point_cloud = shared_point_cloud_memory.find_or_construct<point_cloud>("point_cloud")();
-    while (nh_.ok())
-    {   
-        ROS_WARN ("connected");
-
-        // point_cloud *shared_point_cloud = shared_point_cloud_memory.find<point_cloud>("point_cloud");
-        // const boost::shared_ptr<point_cloud> pc = boost::make_shared<point_cloud>(*shared_point_cloud);
-        // point_cloud *pc_ = (point_cloud *)request.data();
-        // const boost::shared_ptr<point_cloud> pc = boost::make_shared<point_cloud>(*pc_);        
-        // viewer.showCloud(pc);
-        zmq::message_t message;
-        subscriber.recv(&message);
-
-        std::string pc = std::string(static_cast<char*>(message.data()), message.size());
-        ROS_WARN_STREAM("msg " << pc);
-    }
+    return 0;
 }
